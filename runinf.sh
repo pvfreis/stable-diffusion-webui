6d0924d3e5573504f3fc2f0b54157603457a32f4
Paulo Vitor <pvfreis@gmail.com>
2023-03-31 12:08:06 -0300

    Forking for api use, added a few scripts for testing

---
+++ runinf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Replace 'your_script.sh' with the name of your .sh file
+
+./webui-user.sh 2>&1 | tee "$temp_file"
+output=$(cat "$temp_file")
+rm "$temp_file"
+
+# Replace 'success_pattern' with a string or pattern that indicates success in the output
+success_pattern="127.0.0.1"
+
+if echo "$output" | grep -q "$success_pattern"; then
+    echo "Service enabled."
+    # Replace 'your_python_script.py' with the name of your Python script
+    python3 test-api.py
+    echo "Inference ran successfully."
+else
+    echo "Script did not run successfully."
+fi
