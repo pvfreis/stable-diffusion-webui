5f26002404c0f8aaf1551b291d287ef7513b9f72
Paulo Vitor <pvfreis@gmail.com>
2023-03-31 16:32:19 -0300

    Updated test-api for batch prompting

--- test-api.py
+++ test-api.py
@@ -3,35 +3,46 @@ import requests
 import io
 import base64
 import time
+import os
 from PIL import Image, PngImagePlugin
 
 url = "http://127.0.0.1:7861"
 
-payload = {
-    "prompt": "1boy",
-    "steps": 25,
-    "sampler_index":'DPM++ 2M Karras',
-}
+prompts = ["1boy", "1girl", "1dog", "1cat"]  # Add more prompts as needed
 
-while True:
-    try:
-        response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
-        response.raise_for_status()
-        break
-    except requests.exceptions.RequestException as e:
-        print(f"Service not available yet: {e}")
-        time.sleep(5)  # Wait for 5 seconds before retrying
+# Create a timestamped folder
+timestamp = time.strftime("%H-%M-%S")
+os.makedirs(timestamp, exist_ok=True)
 
-r = response.json()
-
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-
-    png_payload = {
-        "image": "data:image/png;base64," + i
+for prompt in prompts:
+    payload = {
+        "prompt": prompt,
+        "steps": 25,
+        "sampler_index": 'DPM++ 2M Karras',
     }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('test/output.png', pnginfo=pnginfo)
+    while True:
+        try:
+            response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+            response.raise_for_status()
+            break
+        except requests.exceptions.RequestException as e:
+            print(f"Service not available yet: {e}")
+            time.sleep(5)  # Wait for 5 seconds before retrying
+
+    r = response.json()
+
+    for i, img_data in enumerate(r['images']):
+        image = Image.open(io.BytesIO(base64.b64decode(img_data.split(",", 1)[0])))
+
+        png_payload = {
+            "image": "data:image/png;base64," + img_data
+        }
+        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+
+        # Save the images with different filenames in the timestamped folder
+        image_filename = f"{timestamp}/{prompt}_{i}.png"
+        image.save(image_filename, pnginfo=pnginfo)
