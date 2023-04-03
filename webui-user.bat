17486a0daff93a9bd25e0eac2fb374a4ca1e3a09
pvfreis <paulovitorfr@Hotmail.com>
2023-04-02 23:22:45 -0300

    Added webui-user.bat to gitignore for local testing

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS= --xformers --medvram --precision full --no-half --no-half-vae --opt-split-attention
+set OPTIMIZED_TURBO=true
 
 call webui.bat
